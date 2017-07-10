a7957170a9dc467427c3f75aee9d10b1f3bb0337
Lien <messagelien@gmail.com>
2017-07-10 23:33:37 +1000

    Scrabble class exists - Test and pass

--- scrabble_test.rb
+++ scrabble_test.rb
@@ -1,2 +1,12 @@
 require 'minitest/autorun'
 require_relative 'scrabble'
+
+
+class ScrabbleTest < Minitest::Test
+
+  def test_scrabble_class_exists
+    @name = Scrabble.new
+    assert_instance_of(Scrabble, @name)
+  end
+
+end
