d671a7d3c07a110905629be581545ebe137f0953
Lien <messagelien@gmail.com>
2017-07-10 23:40:57 +1000

    Refactor - def setup

--- scrabble_test.rb
+++ scrabble_test.rb
@@ -4,13 +4,15 @@ require_relative 'scrabble'
 
 class ScrabbleTest < Minitest::Test
 
-  def test_scrabble_class_exists
+  def setup
     @name = Scrabble.new
+  end
+
+  def test_scrabble_class_exists
     assert_instance_of(Scrabble, @name)
   end
 
   def test_score_method_exists
-    @name = Scrabble.new
     assert_respond_to(@name, :score)
   end
 
